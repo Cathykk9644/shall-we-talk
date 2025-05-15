7433f58abe61738164961e3a0e50a415c5efa6f4
Cathy Hui <erashine414@icloud.com>
2025-05-15 13:47:06 +0800

    Initial backend route and controller setup for testing purpose

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5002;
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
