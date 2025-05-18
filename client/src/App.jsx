497dedb847ece571d2e525b7baac7453b7632599
Cathy Hui <erashine414@icloud.com>
2025-05-18 20:26:12 +0800

    Frontend setup for basic page router, daisyUI etc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import SingUpPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import OnboardingPage from "./pages/OnboardingPage";
+import NotificationPage from "./pages/NotificationPage";
+import ChatPage from "./pages/ChatPage";
+import VideoCallPage from "./pages/VideoCallPage";
 
 const App = () => {
-  return <div className="bg-blue-300 h-screen">App</div>;
+  return (
+    <div className="bg-blue-300 h-screen">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SingUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/notification" element={<NotificationPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/videocall" element={<VideoCallPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
