b32685eb637be104bd0b3c86f266858dafdba8d8
Cathy Hui <erashine414@icloud.com>
2025-06-07 12:30:52 +0800

    Implemented custom hooks for fetching authenticated users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,19 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignupPage";
+import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import OnboardingPage from "./pages/OnboardingPage";
 import NotificationPage from "./pages/NotificationPage";
 import ChatPage from "./pages/ChatPage";
 import VideoCallPage from "./pages/VideoCallPage";
 import { Toaster } from "react-hot-toast";
-import { useQuery } from "@tanstack/react-query";
 
-import { axiosInstance } from "./config/axios.js";
 import PageLoader from "./components/PageLoader.jsx";
+import useAuthUser from "./hooks/useAuthUser.js";
 
 const App = () => {
-  const { data: authData, isLoading } = useQuery({
-    queryKey: ["authenticatedUser"],
-
-    queryFn: async () => {
-      const res = await axiosInstance.get("/auth/me");
-
-      return res.data;
-    },
-    retry: false,
-  });
-
-  const authUser = authData?.user;
+  const { isLoading, authUser } = useAuthUser();
 
   if (isLoading) return <PageLoader />;
 
