53e3bbd5afa4958ea10a911edb76b43a221f4714
Cathy Hui <erashine414@icloud.com>
2025-06-24 23:33:31 +0800

    Server setup for using cloudinary for image upload

---
+++ server/src/config/cloudinary.js
@@ -0,0 +1,18 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadToCloudinary = async (path, folder = "my-avatar") => {
+  try {
+    const data = await cloudinary.uploader.upload(path, { folder: folder });
+    return { url: data.secure_url, publicId: data.public_id };
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+module.exports = { uploadToCloudinary };
