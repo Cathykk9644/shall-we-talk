768c7bc86a4fb6486fb70b6a3efe31cd4e6390d7
Cathy Hui <erashine414@icloud.com>
2025-08-10 16:44:30 +0800

    Removed useeffect on homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,23 +1,8 @@
-import React, { useEffect } from "react";
 import Header from "../components/Header.jsx";
 import Banner from "../components/Banner.jsx";
 import Footer from "../components/Footer.jsx";
 
 const HomePage = () => {
-  // Disable vertical scrolling when this page is mounted
-  useEffect(() => {
-    const prevBodyOverflow = document.body.style.overflowY;
-    const prevHtmlOverflow = document.documentElement.style.overflowY;
-
-    document.body.style.overflowY = "hidden";
-    document.documentElement.style.overflowY = "hidden";
-
-    return () => {
-      document.body.style.overflowY = prevBodyOverflow;
-      document.documentElement.style.overflowY = prevHtmlOverflow || "";
-    };
-  }, []);
-
   return (
     <div className="flex flex-col h-screen bg-bgColor1 overflow-hidden">
       <Header />
