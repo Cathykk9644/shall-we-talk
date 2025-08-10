3e6e9052133b87258fa475917bd1f16929aecabc
Cathy Hui <erashine414@icloud.com>
2025-08-10 15:58:43 +0800

    Delete user account logics updated

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -251,12 +251,11 @@ export async function updateUserProfile(req, res) {
 export async function deleteUserAccount(req, res) {
   try {
     const userId = req.user._id;
-    const deletedUser = await User.findByIdAndDelete(userId);
-    if (!deletedUser)
-      return res.status(404).json({ message: "User not found." });
-    res.status(200).json({ message: "User account deleted successfully." });
+    await User.findByIdAndDelete(userId);
+    // Optionally, you might want to clean up other related data
+    res.status(200).json({ message: "Account deleted successfully" });
   } catch (error) {
-    console.error("Error in deleteUserAccount controller", error.message);
+    console.error("Error deleting user account:", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
